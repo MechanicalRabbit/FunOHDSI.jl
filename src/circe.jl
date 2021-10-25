b0d9a0988adc1d3e8a285d0fee2cc3530f43ec3d
Kirill Simonov <xi@resolvent.net>
2021-10-25 15:52:25 +0300

    updated SqlRender to 1.8.1

--- src/circe.jl
+++ src/circe.jl
@@ -7,7 +7,7 @@ function initialize_java()
         @eval using JavaCall
         Base.invokelatest() do
             JavaCall.addClassPath(joinpath(artifact"CirceR", "CirceR-1.1.1/inst/java/*"))
-            JavaCall.addClassPath(joinpath(artifact"SqlRender", "SqlRender-1.7.0/inst/java/*"))
+            JavaCall.addClassPath(joinpath(artifact"SqlRender", "SqlRender-1.8.1/inst/java/*"))
             JavaCall.init(["-Xmx128M"])
         end
         java_initialized[] = true
